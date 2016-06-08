c6f11103e83b08100e98fc9250659574bde7f293
Niall Robinson <niall.h.robinson@gmail.com>
2016-06-08 13:54:33 +0100

    finished desequencer

--- seqdeseq.py
+++ seqdeseq.py
@@ -1,4 +1,3 @@
-from hadoop.io.SequenceFile import CompressionType
 from hadoop.io import BytesWritable, Text
 from hadoop.io import SequenceFile
 import warnings
@@ -9,6 +8,10 @@ from boto.s3.key import Key
 
 
 def get_s3_file(s3_file, fileout, bucket='informatics-emr-mrjob'):
+    """
+    Downloads an S3 file
+
+    """
     c = boto.connect_s3()
     boto.s3.connect_to_region('eu-west-1')
     bucket = c.get_bucket(bucket)
@@ -18,7 +21,7 @@ def get_s3_file(s3_file, fileout, bucket='informatics-emr-mrjob'):
     return fileout 
 
 
-def write_seq_file(file_out, s3_files_in, tempvaluefile="/tmp/temp.nc"):
+def sequence(file_out, s3_files_in, tempvaluefile="/tmp/temp.nc"):
     """
     String file path to write to
     A list of string file paths to read from. Each file in is encoded to a
@@ -44,3 +47,31 @@ def write_seq_file(file_out, s3_files_in, tempvaluefile="/tmp/temp.nc"):
             value_writer.set(f.read())
             writer.append(key_writer, value_writer)
     writer.close()
+
+
+def desequence(seq_file, output_path, get_fname=lambda k, i: return "file"+str(i)+".nc"):
+    """
+    Takes a sequence file and writes out a separate NetCDF file
+    for each value.
+
+    seq_file: path to a seq file where the values are valid NetCDF binary blobs
+    output_path: a string path to dump files to
+    get_fname: a function which takes the key and an incrimental integer,
+                    and returns a string to be used as the file name.
+
+    """
+    reader = SequenceFile.Reader(seq_file)
+
+    key_class = reader.getKeyClass()
+    value_class = reader.getValueClass()
+
+    key = key_class()
+    value = value_class()
+
+    position = reader.getPosition()
+    i = 0
+    while reader.next(key, value):
+        with open(output_path+get_fname(key, i), "wb") as f:
+            f.write(value.getBytes())
+        i += 1
+    reader.close()
